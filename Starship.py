be7e1cd3e7ca592a3bb1a20802f51c8ee8b839f0
pgonper1211 <114181246+pgonper1211@users.noreply.github.com>
2024-08-07 22:37:26 -0400

    Clases Faltantes

---
+++ Starship.py
@@ -0,0 +1,4 @@
+class Starship:
+    def __init__(self, name, model, manufacturer, cost_in_credits, length, crew, passengers, cargo_capacity, consumables, hyperdrive_rating, MGLT, starship_class, pilots, films):
+        pass
+
